08bda64888ea5e7fc3918630a4659aaf80acd5bc
Carlos_Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2022-01-20 18:42:23 -0300

    Configurando

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordem de Serviço</title>
+
+    <!--CSS-->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/side-bar.css">
+
+</head>
+<body>
+    <div class="side-bar">
+        <div class="logo-detalhes">
+            <i class='bx bx-glasses-alt'></i>
+            <span class="logo-nome">Óptica Núcleo Visão</span>
+        </div>
+
+        <ul class="nav-links">
+            <li>
+                 <a href="#">
+                 <i class="bx bx-grid-alt"></i>
+                    <span class="link-nome">DashBoard</span>
+                </a>
+         </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
